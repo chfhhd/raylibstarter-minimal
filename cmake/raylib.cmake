b4f0464d93815a1536940f9b72d657514cd33084
Christoph Hahn <chrhhn@gmail.com>
2023-04-03 15:22:45 +0200

    Updated Raylib version to 4.5.0

--- cmake/raylib.cmake
+++ cmake/raylib.cmake
@@ -1,7 +1,7 @@
 # Based on: https://raw.githubusercontent.com/raysan5/raylib/master/projects/CMake/CMakeLists.txt
 
 # Set this to the minimal version you want to support
-find_package(raylib 4.0.0 QUIET) # Let CMake search for a raylib-config.cmake
+find_package(raylib 4.5.0 QUIET) # Let CMake search for a raylib-config.cmake
 
 # You could change the QUIET above to REQUIRED and remove this if() clause
 # This part downloads raylib and builds it if it's not installed on your system
@@ -11,7 +11,7 @@ if (NOT raylib_FOUND) # If there's none, fetch and build raylib
     FetchContent_Declare(
             raylib
             # Download a specific release
-            URL https://github.com/raysan5/raylib/archive/refs/tags/4.0.0.tar.gz
+            URL https://github.com/raysan5/raylib/archive/refs/tags/4.5.0.tar.gz
             # Or download current working state
             # URL https://github.com/raysan5/raylib/archive/master.tar.gz
     )
